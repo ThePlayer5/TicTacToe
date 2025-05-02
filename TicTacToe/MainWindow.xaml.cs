74481f46626f00503e061111d2d6054c64a2a1a5
GOOGLE228 <75929954+GOOGLE228@users.noreply.github.com>
2025-05-02 11:03:19 +0300

    Добавил пояснения

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -31,8 +31,8 @@ namespace TicTacToe
             Button btn = sender as Button;
             if (btn.Content != null) return;
 
-            btn.Content = _isXTurn ? "X" : "0";
-            _isXTurn = !_isXTurn; // Отдаём ход ноликам
+            btn.Content = _isXTurn ? "X" : "0"; /// проверяем чья очередь хода
+            _isXTurn = !_isXTurn; /// Отдаём ход ноликам
             CheckWinner();
         }
         private void CheckWinner()
@@ -40,19 +40,21 @@ namespace TicTacToe
             string[,] field = new string[3, 3];
             int index = 0;
 
-            foreach (Button btn in GameGrid.Children) // Chidlren - хранит все дочерние элементы, в данном случае кнопки внутри UniformGrid
+            foreach (Button btn in GameGrid.Children) /// Chidlren - хранит все дочерние элементы, в данном случае кнопки внутри UniformGrid
             {
-                field[index / 3, index % 3] = btn.Content?.ToString();
+                /// Заполняем для того, чтобы обращаться через i и цифры к координатам кнопок и к их содержимому для проверок
+                /// т.к. index может быть от 0 до 8, НО у меня двумерный массив из-за этого / 3 и % 3, чтобы заполнение шло змейкой по field
+                field[index / 3, index % 3] = btn.Content?.ToString(); 
                 index++;
             }
 
             for (int i = 0; i < 3; i++)
             {
-                // rows
+                // Проверяем на равенство столбцы
                 if (field[i, 0] != null && field[i, 0] == field[i, 1] && field[i, 1] == field[i, 2]) ShowWinner(field[i, 0]);
-                // Пишем field[i, 0] для того чтобы положить в место параметра елемент, которыйй стоит по данным координатам, если там 0 или Х выведется победа одной из сторон
+                /// Пишем field[i, 0] для того чтобы положить в место параметра елемент, которыйй стоит по данным координатам, если там 0 или Х выведется победа одной из сторон
 
-                // columns
+                // Проверяем на равенство строки
                 if (field[0, i] != null && field[0, i] == field[1, i] && field[1, i] == field[2, i]) ShowWinner(field[0, i]);
             }
             if (field[0, 0] != null && field[0, 0] == field[1, 1] && field[1, 1] == field[2, 2]) ShowWinner(field[0, 0]);
@@ -60,7 +62,9 @@ namespace TicTacToe
         }
         private void ShowWinner(string winner)
         {
-            MessageBox.Show($"{winner} победил!", "Игра окончена");
+            MessageBox.Show($"{winner} победил!", "Игра окончена"); /// значение winner берётся отсюда ShowWinner(field[i, 0])
+
+            // Очищаем всё поле
             foreach (Button btn in GameGrid.Children)
             {
                 btn.Content = null;
@@ -70,6 +74,7 @@ namespace TicTacToe
 
         private void NewGameBtn_Click(object sender, RoutedEventArgs e)
         {
+            // Логика кнопки очищения поля для ничьи
             foreach (Button btn in GameGrid.Children)
             {
                 btn.Content = null;
